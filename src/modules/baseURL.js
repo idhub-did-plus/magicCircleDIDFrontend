bd232a9c7fb8e4f7ef8165795568b919bbe722cd
laixiaoyin <1012551253@qq.com>
2019-09-25 17:11:11 +0800

    change baseURL

--- src/modules/baseURL.js
+++ src/modules/baseURL.js
@@ -1,4 +1,4 @@
-const thirdParty = "http://api.stplatform.idhub.network:2001";   //第三方登录后台
+const thirdParty = "http://stplatform.idhub.network:2001";   //第三方登录后台
 const baseURL = "http://api.hosting.idhub.network:2003";   //数据托管平台
 
 export default {

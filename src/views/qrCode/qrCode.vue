cb10f3822263a815b8a75b63d9df415097c425a5
laixiaoyin <1012551253@qq.com>
2019-09-28 18:57:44 +0800

    modify base_URL

--- src/views/qrCode/qrCode.vue
+++ src/views/qrCode/qrCode.vue
@@ -82,8 +82,8 @@ export default {
             var jsonObj = {
             "aud":"0x49dba8f906c745b0a82f4d21e02bafd7df1a0be4",
             "sub":"login|authorization|st|did|archive",
-            // "url":window.location.href,
-            "url":"http://stplatform.idhub.network",
+            "url":window.location.href,
+            // "url":"http://stplatform.idhub.network",
             "rdt":url.thirdParty+"/v1/did/token/",
             "jti":this.uuid
             }
@@ -132,8 +132,8 @@ export default {
                 var jsonObj = {
                 "aud":"0x49dba8f906c745b0a82f4d21e02bafd7df1a0be4",
                 "sub":"login|authorization|st|did|archive",
-                // "url":window.location.href,
-                "url":"http://stplatform.idhub.network",
+                "url":window.location.href,
+                // "url":"http://stplatform.idhub.network",
                 "rdt":url.thirdParty+"/v1/did/token/",
                 "jti":this.uuid
                 }
